59723b29fa986a2fbdcc198b0d05fc804eef7d18
Jakub Glod <jakub.glod1@gmail.com>
2026-01-14 22:17:10 +0100

    Small changes

--- DarWInWOrld/src/main/java/project/model/Simulation/Simulation.java
+++ DarWInWOrld/src/main/java/project/model/Simulation/Simulation.java
@@ -2,12 +2,14 @@ package project.model.Simulation;
 
 import project.model.RectangularMap;
 import project.model.WorldElements.Animal;
+import project.model.WorldElements.EdibleElements.Plant;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Simulation {
     private List<Animal> animals = new ArrayList<Animal>();
+    private List<Plant> plants = new ArrayList<Plant>();
     private RectangularMap worldMap;
 
     public Simulation(SimulationParameters parameters) {
